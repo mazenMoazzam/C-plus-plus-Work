7a2ada7a0d4e459bf84f45a76ffc613d808a1b15
“Mazen <maz.moazzam345@gmail.com>
2026-05-07 23:53:11 -0400

    Adding server.cpp file to create broker and gitignore

---
+++ broker/server.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        std::cerr << "Failed to create socket\n";
+        return 1;
+    }
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(9092);
+
+    if (bind(server_fd, (sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "Bind failed\n";
+        close(server_fd);
+        return 1;
+    }
+
+    if (listen(server_fd, 5) < 0) {
+        std::cerr << "Listen failed\n";
+        close(server_fd);
+        return 1;
+    }
+
+    std::cout << "Broker listening on port 9092...\n";
+
+    sockaddr_in client_addr{};
+    socklen_t client_len = sizeof(client_addr);
+    int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
+    
+    if (client_fd < 0) {
+        std::cerr << "Accept failed\n";
+        close(server_fd);
+        return 1;
+    }
+
+    std::cout << "Client connected!\n";
+
+    char buffer[1024] = {0};
+    int bytes_read = read(client_fd, buffer, sizeof(buffer));
+    
+    if (bytes_read > 0) {
+        std::cout << "Received: " << buffer << "\n";
+    }
+
+    close(client_fd);
+    close(server_fd);
+
+    return 0;
+}
